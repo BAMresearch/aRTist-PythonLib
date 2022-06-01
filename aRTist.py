ede978bf17e6ab5d6708883e38b5140be36981e8
dschumac <david.schumacher@bam.de>
2022-06-01 17:39:00 +0200

    Subgroups added for better orientation in file. Reordered functions.

--- aRTist.py
+++ aRTist.py
@@ -27,8 +27,6 @@ class Connection:
     def send(self, x):
         total = ""
         for i in range(len(x)):
-           # print(i+1, "of", len(x), " commands send")  
-           # print(x[i])
             self.S.send(x[i].encode())
             total = total + self.listen(1)
         return total
@@ -68,171 +66,10 @@ class Connection:
         self.S.settimeout(self.timeout)
         return total
 
-''' Scene '''
+
+############################### Scene #########################################
 def open_scene(Path):
     STR = ["""FileIO::OpenAny """+ Path +""";
-           """]
-    return STR
-
-def SETUP_Preview(state):
-    #state (0, 1)
-    STR = ["""set ::Engine::RenderPreview """+str(state)+""";
-           """]
-    return STR
-
-def RenderPreview():
-    STR = ["""::Engine::RenderPreview;
-           """]
-    return STR
-
-
-def SETUP_detector(posZ, angleZ, x, y, px, RefPoint, GV): #Order of commands important!! -> Invoke should be reomved!
-    #RefPoint ('off', 'min', 'max', 'center')
-    STR = ["""::PartList::Invoke D SetPosition 0 0 """+ str(posZ) +""";
-           ""","""::PartList::Invoke D SetRefPos 0 0 """+ str(posZ) +""";
-           ""","""::PartList::Invoke D SetOrientation 0 0 """+ str(angleZ) +""";
-           ""","""set ::Xdetector(AutoD) """+ RefPoint +""";
-           ""","""set ::Xdetector(RefGV) """+ str(GV) +""";
-           ""","""::XDetector::ExposureModeChange;
-           ""","""set ::Xsetup_private(DGauto) Size;
-           ""","""set ::Xsetup(DetectorPixelX) """+ str((int(x))) +""";
-           ""","""set ::Xsetup(DetectorPixelY) """+ str((int(y))) +""";
-           ""","""set ::Xsetup_private(DGdx) """+ str(px) +""";
-           ""","""set ::Xsetup_private(DGdy) """+ str(px) +""";
-           ""","""::XDetector::UpdateGeometry 1;
-           """]
-    return STR
-
-def SETUP_source(posZ, kV, mA, Type, FMat, FThick):
-    #Type (Mono, General)
-    STR = ["""set ::Xsource(Exposure) """+ str(mA) +""";
-           ""","""set ::Xsource(Voltage) """+ str(kV) +""";
-           ""","""set ::Xsource(Tube) """+ str(Type) +""";
-           ""","""set ::Xsource(FilterMaterial) """+ str(FMat) +""";
-           ""","""set ::Xsource(FilterThickness) """+ str(FThick) +""";
-           ""","""::PartList::Invoke S SetPosition 0 0 """+ str(posZ) +""";
-           ""","""::PartList::Invoke S SetRefPos 0 0 """+ str(posZ) +""";
-           ""","""::XSource::ComputeSpectrum;
-           """]
-    return STR
-
-def SETUP_SourceSampling(sampling):
-    STR = ["""set ::Xsetup(SourceSampling) """+ str(sampling) +""";
-           ""","""::XSource::SelectSpotTypeEvent;
-           """]
-    return STR
-
-def SETUP_CalcFocalspot(width, height, fract, res):
-    STR = ["""set ::Xsource_private(SpotWidth) """+ str(width) +""";       
-			""","""set ::Xsource_private(SpotHeight) """+ str(height) +""";
-			""","""set ::Xsource_private(SpotHeight) """+ str(height) +""";
-			""","""set ::Xsource_private(SpotLorentz) """+ str(fract) +""";
-			""","""set ::Xsource_private(SpotRes) """+ str(res) +""";
-			""","""::XSource::SetSpotProfile;
-			"""]
-    return STR
-
-def LOAD_spectrum(SpecPath):
-    #Type (Mono, General)
-    STR = ["""::XSource::LoadSpectrum """+ SpecPath +""";
-           """]
-    return STR
-
-def Acq_FF():
-    STR = ["""::XDetector::FFCorrGenCmd;
-           """]
-    return STR
-
-def AutoAcq_FF(state):
-    #state (0, 1)
-    STR = ["""set ::Xdetector(FFCorrRun) """+str(state)+""";
-           """]
-    return STR
-
-def Clear_FF():
-    STR = ["""XDetector::FFCorrClearCmd;
-           """]
-    return STR
-
-def count_parts():
-    STR = ["""::PartList::Count;
-           """]
-    return STR
-
-def set_material(ID, mat):
-    STR = ["""::PartList::Set """+ str(ID) +""" Material """+ mat +""";
-           """]
-    return STR
-
-def set_position(ID, x, y, z):
-    STR = ["""::PartList::Invoke """+ str(ID) +""" SetPosition """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
-           ""","""::PartList::Invoke """+ str(ID) +""" SetRefPos """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
-           """]
-    return STR
-
-def translate(ID, x, y, z):
-    STR = ["""::PartList::Invoke """+ str(ID) +""" Translate world """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
-           """]
-    return STR
-
-def set_vis_AllParts(state): #state = "on"/"off"
-    STR = ["""::PartList::SetVisibility """+state+""";
-           """]
-    return STR
-
-def set_vis(ID, state):
-    STR = ["""::PartList::Set """+str(ID)+""" Visible """+state+""";
-           """]
-    return STR
-
-def resize(ID, size):
-    STR = ["""::PartList::Invoke """+str(ID)+""" SetSize """+str(size)+""" """+str(size)+""" """+str(size)+""";
-           """]
-    return STR
-
-def make_image():
-    STR = ["""::Engine::StartStopCmd;
-           """]
-    return STR
-
-def save_image(Path):
-    STR = ["""::Modules::Execute ImageViewer Save16bit """+ Path +""" 1;
-           """]
-    return STR
-
-def SETUP_CT(CTModNo, angle, steps, OutDir, OutName, OutFormat, PType, RunFDK, OnlySelObj, Direction, Scat, ScatInt):
-    #OutFormat ({BAM CT}, TIFFs)
-    #PType (16bit, float)
-    #Direction (clockwise, counterclockwise)
-    #Scat (off, McRay)
-    STR = ["""::Modules::Run CtScan;
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Angle) """+ str(angle) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Steps) """+ str(steps) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Directory) """+ OutDir +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(File) """+ OutName +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Output) """+ str(OutFormat) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(PixelType) """+ str(PType) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Feldkamp) """+ str(RunFDK) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Selected) """+ str(OnlySelObj) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Direction) """+ str(Direction) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(ScatterMode) """+ Scat +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(ScatterInterval) """+ str(ScatInt) +""";
-           """]
-    return STR
-
-def SETUP_FDK(CTModNo, VType, Interp, OutFormat):
-    #VType (8bit, 16bit, 32bit, float)
-    #OutFormat ({BAM CT}, VTK, RAW)
-    STR = ["""::Modules::Run CtScan;
-        ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(VoxelType) """+ str(VType) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Interpolate) """+ str(Interp) +""";
-           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(FileType) """+ str(OutFormat) +""";
-           """]
-    return STR
-
-def Run_CT(CTModNo):
-    STR = ["""::Modules::Run CtScan;
-        ""","""::Modules::Module""" +str(CTModNo)+"""::Execute; 
            """]
     return STR
 
@@ -275,6 +112,177 @@ def delete_all_Obj():
     STR = ["""PartList::Clear;
            """]
     return STR
+
+def count_parts():
+    STR = ["""::PartList::Count;
+           """]
+    return STR
+
+def set_material(ID, mat):
+    STR = ["""::PartList::Set """+ str(ID) +""" Material """+ mat +""";
+           """]
+    return STR
+
+def set_position(ID, x, y, z):
+    STR = ["""::PartList::Invoke """+ str(ID) +""" SetPosition """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
+           ""","""::PartList::Invoke """+ str(ID) +""" SetRefPos """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
+           """]
+    return STR
+
+def translate(ID, x, y, z):
+    STR = ["""::PartList::Invoke """+ str(ID) +""" Translate world """+ str(x) +""" """+ str(y) +""" """+ str(z) +""";
+           """]
+    return STR
+
+def set_vis_AllParts(state): #state = "on"/"off"
+    STR = ["""::PartList::SetVisibility """+state+""";
+           """]
+    return STR
+
+def set_vis(ID, state):
+    STR = ["""::PartList::Set """+str(ID)+""" Visible """+state+""";
+           """]
+    return STR
+
+def resize(ID, size):
+    STR = ["""::PartList::Invoke """+str(ID)+""" SetSize """+str(size)+""" """+str(size)+""" """+str(size)+""";
+           """]
+    return STR
+
+
+############################### Detector #########################################
+def SETUP_detector(posZ, angleZ, x, y, px, RefPoint, GV): #Order of commands important!! -> Invoke should be reomved!
+    #RefPoint ('off', 'min', 'max', 'center')
+    STR = ["""::PartList::Invoke D SetPosition 0 0 """+ str(posZ) +""";
+           ""","""::PartList::Invoke D SetRefPos 0 0 """+ str(posZ) +""";
+           ""","""::PartList::Invoke D SetOrientation 0 0 """+ str(angleZ) +""";
+           ""","""set ::Xdetector(AutoD) """+ RefPoint +""";
+           ""","""set ::Xdetector(RefGV) """+ str(GV) +""";
+           ""","""::XDetector::ExposureModeChange;
+           ""","""set ::Xsetup_private(DGauto) Size;
+           ""","""set ::Xsetup(DetectorPixelX) """+ str((int(x))) +""";
+           ""","""set ::Xsetup(DetectorPixelY) """+ str((int(y))) +""";
+           ""","""set ::Xsetup_private(DGdx) """+ str(px) +""";
+           ""","""set ::Xsetup_private(DGdy) """+ str(px) +""";
+           ""","""::XDetector::UpdateGeometry 1;
+           """]
+    return STR
+
+def Acq_FF():
+    STR = ["""::XDetector::FFCorrGenCmd;
+           """]
+    return STR
+
+def AutoAcq_FF(state):
+    #state (0, 1)
+    STR = ["""set ::Xdetector(FFCorrRun) """+str(state)+""";
+           """]
+    return STR
+
+def Clear_FF():
+    STR = ["""XDetector::FFCorrClearCmd;
+           """]
+    return STR
+
+############################### Source ########################################
+def SETUP_source(posZ, kV, mA, Type, FMat, FThick):
+    #Type (Mono, General)
+    STR = ["""set ::Xsource(Exposure) """+ str(mA) +""";
+           ""","""set ::Xsource(Voltage) """+ str(kV) +""";
+           ""","""set ::Xsource(Tube) """+ str(Type) +""";
+           ""","""set ::Xsource(FilterMaterial) """+ str(FMat) +""";
+           ""","""set ::Xsource(FilterThickness) """+ str(FThick) +""";
+           ""","""::PartList::Invoke S SetPosition 0 0 """+ str(posZ) +""";
+           ""","""::PartList::Invoke S SetRefPos 0 0 """+ str(posZ) +""";
+           ""","""::XSource::ComputeSpectrum;
+           """]
+    return STR
+
+def SETUP_SourceSampling(sampling):
+    STR = ["""set ::Xsetup(SourceSampling) """+ str(sampling) +""";
+           ""","""::XSource::SelectSpotTypeEvent;
+           """]
+    return STR
+
+def SETUP_CalcFocalspot(width, height, fract, res):
+    STR = ["""set ::Xsource_private(SpotWidth) """+ str(width) +""";       
+			""","""set ::Xsource_private(SpotHeight) """+ str(height) +""";
+			""","""set ::Xsource_private(SpotHeight) """+ str(height) +""";
+			""","""set ::Xsource_private(SpotLorentz) """+ str(fract) +""";
+			""","""set ::Xsource_private(SpotRes) """+ str(res) +""";
+			""","""::XSource::SetSpotProfile;
+			"""]
+    return STR
+
+def LOAD_spectrum(SpecPath):
+    #Type (Mono, General)
+    STR = ["""::XSource::LoadSpectrum """+ SpecPath +""";
+           """]
+    return STR
+
+################################# CT ##########################################
+def SETUP_CT(CTModNo, angle, steps, OutDir, OutName, OutFormat, PType, RunFDK, OnlySelObj, Direction, Scat, ScatInt):
+    #OutFormat ({BAM CT}, TIFFs)
+    #PType (16bit, float)
+    #Direction (clockwise, counterclockwise)
+    #Scat (off, McRay)
+    STR = ["""::Modules::Run CtScan;
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Angle) """+ str(angle) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Steps) """+ str(steps) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Directory) """+ OutDir +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(File) """+ OutName +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Output) """+ str(OutFormat) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(PixelType) """+ str(PType) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Feldkamp) """+ str(RunFDK) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Selected) """+ str(OnlySelObj) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Direction) """+ str(Direction) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(ScatterMode) """+ Scat +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(ScatterInterval) """+ str(ScatInt) +""";
+           """]
+    return STR
+
+def SETUP_FDK(CTModNo, VType, Interp, OutFormat):
+    #VType (8bit, 16bit, 32bit, float)
+    #OutFormat ({BAM CT}, VTK, RAW)
+    STR = ["""::Modules::Run CtScan;
+        ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(VoxelType) """+ str(VType) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(Interpolate) """+ str(Interp) +""";
+           ""","""set ::Modules::Module""" +str(CTModNo)+"""::CtScan(FileType) """+ str(OutFormat) +""";
+           """]
+    return STR
+
+def Run_CT(CTModNo):
+    STR = ["""::Modules::Run CtScan;
+        ""","""::Modules::Module""" +str(CTModNo)+"""::Execute; 
+           """]
+    return STR
+
+
+
+############################### Engine ########################################
+def SETUP_Preview(state):
+    #state (0, 1)
+    STR = ["""set ::Engine::RenderPreview """+str(state)+""";
+           """]
+    return STR
+
+def RenderPreview():
+    STR = ["""::Engine::RenderPreview;
+           """]
+    return STR
+
+def make_image():
+    STR = ["""::Engine::StartStopCmd;
+           """]
+    return STR
+
+def save_image(Path):
+    STR = ["""::Modules::Execute ImageViewer Save16bit """+ Path +""" 1;
+           """]
+    return STR
+
+
+
 
 
 
