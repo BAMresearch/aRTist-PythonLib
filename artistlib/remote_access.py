26c52bbcbe5fb06fad2ed6bc2b9f102762bf85c6
David Denkler <123733758+daviddenkler@users.noreply.github.com>
2023-03-28 19:41:43 +0200

    small layout changes for console

---
+++ artistlib/remote_access.py
@@ -0,0 +1,42 @@
+import artistlib as a
+from prompt_toolkit import PromptSession
+session = PromptSession()
+
+list = ["SUCCESS", "RESULT", "STDOUT"]
+commands = []
+
+rc = a.Junction()
+
+com = ""
+
+c = 0
+
+while com != "exit":
+    com = session.prompt("Command: ")
+    
+    ver = a.Junction.send(rc, com, "*")
+
+    for i in list:
+        typ = a.Junction.pick(rc, ver, i)
+        if "IMAGE" in ver and not "{}" in typ:
+            name = input("Save Image as: ")
+            a.Junction.image(rc, name)
+            print("Image saved as ", name)
+            
+        if not "{}" in typ:
+            if not "not found" in typ:
+                print(typ)
+            else:
+                c += 1
+        else:
+            c += 1
+
+    if c >= 3 and not "BASE64" in ver:
+        print(ver)
+
+    c = 0
+
+else:
+    exit()
+
+
