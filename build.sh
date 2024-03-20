cdf97238a2fb53481b49df1bda135789641f7c0e
Anton Weiss <anton.weiss@th-deg.de>
2024-03-20 07:40:28 +0100

    added error message

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -R dist
-rm -R artistlib.egg-info
-python -m build
-
-# twine upload dist/*
--- build_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Builds the HTML documentation using pdoc3
-# and moves it to the "docs" folder (instead of docs/artistlib).
-pdoc --force --html --template-dir "docs/templates" -o "docs/" artistlib
-cp -R docs/artistlib/* docs/
-rm -R docs/artistlib
-###pdoc --logo "artistlib.png" --template-dir "docs/templates" -o "docs/" artistlib
-###rm -R docs/artistlib
